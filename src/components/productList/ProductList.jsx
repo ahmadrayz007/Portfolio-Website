d3569759e63ce1583687937a42cc23be3e6ece62
ahmadrayz007@gmail.com <ahmadrayz007>
2021-11-14 20:12:13 +0700

    add rto repo

---
+++ src/components/productList/ProductList.jsx
@@ -0,0 +1,28 @@
+import Product from "../Product/Product"
+import { products } from "../../data"
+import "./ProductList.css"
+
+function ProductList() {
+    return (
+        <div>
+            <div className="pl">
+                <div className="pl-texts">
+                    <h1 className="pl-title">Create & inspire. It's Lama</h1>
+                    <p className="pl-desc">
+                    Lama is a creative portfolio that your work has been waiting for.
+                    Beautiful homes, stunning portfolio styles & a whole lot more awaits
+                    inside.
+                    </p>
+                </div>
+                <div className="pl-list">
+                    {products.map(item=>(
+                        <Product key={item.id}img={item.img} link={item.link}/>
+                    ))}
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductList
---
+++ src/data.js
@@ -0,0 +1,18 @@
+
+export const products = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/FXcppyt/Hola-Spatial-GIS-and-Mapping-Analytics-3.png",
+      link: "http://learn.holaspatial.com",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/7rQXH4k/Home-Jasa-Sewa-Alat-Berat-Sumatera-Jawa-2.png",
+      link: "http://learn.holaspatial.com",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/C8KqX2b/Learn-Hola-GIS-and-Mapping-Analytics.png",
+      link: "http://learn.holaspatial.com",
+    }
+  ];
